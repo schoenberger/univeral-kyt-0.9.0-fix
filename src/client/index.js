4c898fa446d643ab82892e140d7b9ce80b73bb19
Christian Schönberger <c.schoenberger@pons.de>
2017-11-14 18:04:20 +0100

    proposed fix

--- src/client/index.js
+++ src/client/index.js
@@ -4,15 +4,23 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './Root';
 
+import match from 'react-router/lib/match';
+import Router from 'react-router/lib/Router';
+import browserHistory from 'react-router/lib/browserHistory';
+import routes from '../routes';
+
 const root = document.querySelector('#root');
 
 const mount = RootComponent => {
-  render(
-    <AppContainer>
-      <RootComponent />
-    </AppContainer>,
-    root
-  );
+  // https://github.com/ReactTraining/react-router/blob/v2.6.1/docs/guides/ServerRendering.md#async-routes
+  match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
+    render(
+      <AppContainer>
+        <Router {...renderProps} />
+      </AppContainer>,
+      root
+    );
+  });
 };
 
 if (module.hot) {
--- src/server/template.js
+++ src/server/template.js
@@ -24,7 +24,7 @@ export default vo => `
   </head>
 
   <body>
-    <div id="root"><div>${vo.root}</div></div>
+    <div id="root">${vo.root}</div>
     ${getDeferScript(vo.manifestJSBundle)}
     ${getDeferScript(vo.vendorJSBundle)}
     ${getDeferScript(vo.mainJSBundle)}
